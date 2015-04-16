ede0a032e7e8365eb88b456debb687c398e8976c
Sean Dolphin <Sean.Dolphin@kpcompass.com>
2015-04-16 10:22:03 -0700

    added NoBlock to execute and ignore a cog requries go 1.4 now because of it

---
+++ wait/run.go
@@ -0,0 +1,14 @@
+package wait
+
+import (
+	"github.com/cogger/cogger"
+	"golang.org/x/net/context"
+)
+
+//NoBlock runs a cog and shallows all errors and blockers
+func NoBlock(ctx context.Context, cog cogger.Cog) {
+	go func() {
+		for range cog.Do(ctx) {
+		}
+	}()
+}
