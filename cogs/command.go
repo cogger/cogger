196ef1dd4b03b28616734f76a3b8858dd5fba2ae
Sean Dolphin <Sean.Dolphin@kpcompass.com>
2015-05-18 11:19:14 -0700

    updated for vet

--- cogs/command.go
+++ cogs/command.go
@@ -8,10 +8,12 @@ import (
 	"golang.org/x/net/context"
 )
 
+//Command creates a cog that runs the command with the arguments provided
 func Command(ctx context.Context, command string, arg ...string) cogger.Cog {
 	return ExecuteCommand(ctx, exec.Command(command, arg...))
 }
 
+//CommandWithOutput creates a cog that runs the command with the arguments provided outputting the return from the command to the buffer
 func CommandWithOutput(ctx context.Context, out *bytes.Buffer, command string, arg ...string) cogger.Cog {
 	cmd := exec.Command(command, arg...)
 	cmd.Stdout = out
@@ -19,6 +21,7 @@ func CommandWithOutput(ctx context.Context, out *bytes.Buffer, command string, a
 	return ExecuteCommand(ctx, cmd)
 }
 
+//ExecuteCommand creates a cog that runs the cmd provided
 func ExecuteCommand(ctx context.Context, cmd *exec.Cmd) cogger.Cog {
 	return Simple(ctx, func() error {
 		return cmd.Run()
--- cogs/returnErr.go
+++ cogs/returnErr.go
@@ -2,6 +2,7 @@ package cogs
 
 import "github.com/cogger/cogger"
 
+//ReturnErr creates a cog that instantly returns the error given to it
 func ReturnErr(err error) cogger.Cog {
 	return cogger.NewCog(func() chan error {
 		out := make(chan error, 1)
